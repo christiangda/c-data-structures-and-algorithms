04ef4955fb101b532cbeaee0e74c9a45aba5dea1
Christian González Di Antonio <christiangda@gmail.com>
2024-03-09 21:34:38 +0100

    feat: linked list

---
+++ linkedlist/src/linked_list.c
@@ -0,0 +1,70 @@
+#include "../include/linked_list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+List *list_new()
+{
+  List *l = (List *)malloc(sizeof(List));
+  l->head = NULL;
+  l->tail = NULL;
+  l->size = 0;
+
+  return l;
+}
+
+void list_destroy(List *list)
+{
+  if (list->head == NULL)
+  {
+    free(list);
+    return;
+  }
+
+  Node *temp_node = NULL;
+
+  while (list->head != NULL)
+  {
+    temp_node = list->head;
+    list->head = temp_node->next;
+
+    free(temp_node->data);
+    free(temp_node);
+  }
+
+  free(list);
+}
+
+void list_prepend(List *list, Node *node)
+{
+  if (list == NULL || node == NULL)
+  {
+    return;
+  }
+
+  // store the pointer to the first element prepend to the list
+  if (list->size == 0)
+  {
+    list->tail = node;
+  }
+
+  node->next = list->head;
+  list->head = node;
+  list->size++;
+}
+
+size_t list_size(List *list)
+{
+  return list->size;
+}
+
+void list_prepend_value(List *list, void *value, size_t size)
+{
+  Node *node = malloc(sizeof(Node));
+  node->next = NULL;
+
+  node->data = malloc(size);
+  node->data = value;
+  node->size = size;
+
+  list_prepend(list, node);
+}
---
+++ linkedlist/tests/main.c
@@ -0,0 +1,100 @@
+#include "../include/linked_list.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void test_list_new()
+{
+  List *list = list_new();
+
+  assert(list->head == NULL);
+  assert(list->size == 0);
+}
+
+void test_list_size_new()
+{
+  List *list = list_new();
+
+  assert(list_size(list) == 0);
+}
+
+void test_prepend_to_new_list()
+{
+  List *list = list_new();
+  Node *node = malloc(sizeof(Node));
+  node->next = NULL;
+
+  list_prepend(list, node);
+
+  assert(list_size(list) == 1);
+}
+
+void test_prepend_10()
+{
+  List *list = list_new();
+
+  for (int i = 0; i < 10; i++)
+  {
+    Node *node = malloc(sizeof(Node));
+    node->next = NULL;
+    list_prepend(list, node);
+  }
+
+  assert(list_size(list) == 10);
+}
+
+void test_list_destroy_10()
+{
+  List *list = list_new();
+
+  for (int i = 0; i < 10; i++)
+  {
+    Node *node = malloc(sizeof(Node));
+    node->next = NULL;
+    list_prepend(list, node);
+  }
+
+  assert(list_size(list) == 10);
+
+  list_destroy(list);
+}
+
+void test_list_prepend_value()
+{
+  List *list = list_new();
+
+  for (int i = 10; i > 0; i--)
+  {
+    int *val = malloc(sizeof(int));
+    *val = i;
+    list_prepend_value(list, val, sizeof(int));
+  }
+
+  Node *temp_node = list->head;
+  for (int i = 0; i < 10; i++)
+  {
+    // int *val = (int *)temp_node->data;
+    // printf("value = %d, size = %zu (bytes)\n", *val, temp_node->size);
+
+    assert(sizeof(int) == temp_node->size);
+    temp_node = temp_node->next;
+  }
+
+  assert(list_size(list) == 10);
+  list_destroy(list);
+}
+
+void tests_run_all(void)
+{
+  test_list_new();
+  test_list_size_new();
+  test_prepend_to_new_list();
+  test_prepend_10();
+  test_list_destroy_10();
+  test_list_prepend_value();
+}
+
+int main(void)
+{
+  tests_run_all();
+}
